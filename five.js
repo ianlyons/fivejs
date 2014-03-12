d15f3ae2950a2177162a928f9989f4f1c654a6cd
ian lyons <ilyons@gmail.com>
2014-03-12 07:32:33 -0700

    Cleaned up grammar on error reporting
    A quick grammar clean-up on the error reporting.

--- five.js
+++ five.js
@@ -3,7 +3,7 @@ Fivejs = {};
 Fivejs.five = function(num) {
   try {
     if (typeof num !== 'number') {
-      throw "invalid input to FiveJs.five() " + num + ' is not a mnumber.';
+      throw "Invalid input to FiveJs.five(): " + num + ' is not a number.';
     } else {
       return num*5;
     }
